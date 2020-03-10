6a6158bb98080615a14061bb4374578e4a5aeb9c
gabriel wagner <gabriel.wagner@ilegra.com>
2020-03-09 23:14:09 -0300

    Adiciona react-navigation v5

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import 'react-native-gesture-handler';
 
 import '~/config/ReactotronConfig';
+import Mapa from '~/pages/Mapa';
+import Main from '~/pages/Main';
 
-import Routes from '~/routes';
+const Stack = createStackNavigator();
 
-const App = () => <Routes />;
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Main} />
+        <Stack.Screen name="Mapa" component={Mapa} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default App;
